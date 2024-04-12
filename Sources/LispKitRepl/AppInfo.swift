c92c43bb671a5cc4a13c1826865942ef43275646
Matthias Zenger <matthias@objecthub.net>
2024-04-12 23:32:45 +0200

    Migrate to NumberKit 2.5. This increases the minimum platform requirements to macOS 13.3.

--- Sources/LispKitRepl/AppInfo.swift
+++ Sources/LispKitRepl/AppInfo.swift
@@ -45,7 +45,7 @@ public struct AppInfo {
   public static let buildDate = { () -> String in
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd HH:mm"
-    return dateFormatter.string(from: Date(timeIntervalSince1970: 1705349418))
+    return dateFormatter.string(from: Date(timeIntervalSince1970: 1712957435))
   }()
   public static let buildAnnotation = " (\(AppInfo.buildDate))"
 }
